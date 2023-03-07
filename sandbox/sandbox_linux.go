ceb6b0467ec5b91ee77abae5e65e585580bf94d0
Mzack9999 <mzack9999@protonmail.com>
2023-03-07 22:07:32 +0100

    adding linux prototype via systemd-run

---
+++ sandbox/sandbox_linux.go
@@ -0,0 +1,161 @@
+//go:build linux
+
+package sandbox
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/projectdiscovery/gozero/command"
+	stringsutil "github.com/projectdiscovery/utils/strings"
+)
+
+type Configuration struct {
+	Rules []Rule
+}
+
+type Filter string
+
+const (
+	PrivateTmp              Filter = "PrivateTmp"
+	PrivateNetwork          Filter = "PrivateNetwork"
+	SELinuxContext          Filter = "SELinuxContext"
+	NoNewPrivileges         Filter = "NoNewPrivileges"
+	ProtectSystem           Filter = "ProtectSystem"
+	ProtectHome             Filter = "ProtectHome"
+	ProtectDevices          Filter = "ProtectDevices"
+	CapabilityBoundingSet   Filter = "CapabilityBoundingSet"
+	ReadWriteDirectories    Filter = "ReadWriteDirectories"
+	ReadOnlyDirectories     Filter = "ReadOnlyDirectories"
+	InaccessibleDirectories Filter = "InaccessibleDirectories"
+	ProtectKernelTunables   Filter = "InaccessibleDirectories"
+	ProtectKernelModules    Filter = "ProtectKernelModules"
+	ProtectControlGroups    Filter = "ProtectControlGroups"
+	RestrictNamespaces      Filter = "RestrictNamespaces"
+	MemoryDenyWriteExecute  Filter = "MemoryDenyWriteExecute"
+	RestrictRealtime        Filter = "RestrictRealtime"
+	PrivateMounts           Filter = "PrivateMounts"
+	DynamicUser             Filter = "DynamicUser"
+	SystemCallFilter        Filter = "SystemCallFilter"
+)
+
+type ArgsType uint8
+
+const (
+	Bool ArgsType = iota
+	Folders
+	Capabilities
+	Namespaces
+	SystemCalls
+)
+
+type Arg struct {
+	Type   ArgsType
+	Params interface{}
+}
+
+type Rule struct {
+	Filter Filter
+	Arg    Arg
+}
+
+// Sandbox native on linux
+type SandboxLinux struct {
+	Config *Configuration
+	conf   []string
+}
+
+// New sandbox with the given configuration
+func New(ctx context.Context, config *Configuration) (Sandbox, error) {
+	if ok, err := IsInstalled(context.Background()); err != nil || !ok {
+		return nil, errors.New("sandbox feature not installed")
+	}
+
+	conf := []string{"--pipe", "--pty", "--user"}
+	for _, rule := range config.Rules {
+		var actionArgs []string
+		if rule.Filter == "" {
+			return nil, errors.New("empty action")
+		}
+		actionArgs = append(actionArgs, "-p")
+		switch rule.Arg.Type {
+		case Bool:
+			v, ok := rule.Arg.Params.(string)
+			if !ok {
+				return nil, errors.New("invalid string value")
+			}
+			if !stringsutil.EqualFoldAny(v, "yes", "no") {
+				return nil, errors.New("invalid value (yes/no)")
+			}
+			actionArgs = append(actionArgs, string(rule.Filter)+"="+v)
+		case Folders, Capabilities, Namespaces, SystemCalls:
+			v, ok := rule.Arg.Params.([]string)
+			if !ok {
+				return nil, errors.New("invalid string value")
+			}
+			actionArgs = append(actionArgs, string(rule.Filter)+"="+strings.Join(v, ","))
+		default:
+			return nil, errors.New("unsupported type")
+		}
+		conf = append(conf, actionArgs...)
+	}
+
+	s := &SandboxLinux{Config: config, conf: conf}
+	return s, nil
+}
+
+func (s *SandboxLinux) Run(ctx context.Context, cmd string) (*command.Result, error) {
+	var params []string
+	params = append(params, s.conf...)
+	params = append(params, strings.Split(cmd, " ")...)
+	log.Fatal(params)
+	cmdContext := exec.CommandContext(ctx, "systemd-run", params...)
+	var stdout, stderr bytes.Buffer
+	cmdContext.Stdout = &stdout
+	cmdContext.Stderr = &stderr
+	return &command.Result{Stdout: stdout.String(), Stderr: stderr.String()}, cmdContext.Run()
+}
+
+// Start the instance
+func (s *SandboxLinux) Start() error {
+	return errors.New("not implemented")
+}
+
+// Wait for the instance
+func (s *SandboxLinux) Wait() error {
+	return errors.New("not implemented")
+}
+
+// Stop the instance
+func (s *SandboxLinux) Stop() error {
+	return errors.New("not implemented")
+}
+
+// Clear the instance after stop
+func (s *SandboxLinux) Clear() error {
+	return errors.New("not implemented")
+}
+
+func isEnabled(ctx context.Context) (bool, error) {
+	return isInstalled(ctx)
+}
+
+func isInstalled(ctx context.Context) (bool, error) {
+	_, err := exec.LookPath("systemd-run")
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func activate(ctx context.Context) (bool, error) {
+	return false, errors.New("sandbox is a linux native functionality")
+}
+
+func deactvate(ctx context.Context) (bool, error) {
+	return false, errors.New("	 can't be disabled")
+}
